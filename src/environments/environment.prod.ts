30c668efe94d34372e96f897cb937a5bff37de87
yiheng <yiheng.chu123@gmail.com>
2017-12-28 12:02:50 -0500

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyApiCAiX6V4upZI3Ek5vV1OTatCDI6FWHY",
+    authDomain: "ecommerce-c1146.firebaseapp.com",
+    databaseURL: "https://ecommerce-c1146.firebaseio.com",
+    projectId: "ecommerce-c1146",
+    storageBucket: "ecommerce-c1146.appspot.com",
+    messagingSenderId: "812110488430"
+  }
 };
